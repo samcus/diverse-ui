0e11ea334b6b1b008515d0074ae3a5494359a089
samcus <samuel.a.custer@gmail.com>
2016-09-23 23:11:10 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # diverse-ui
-Node API for Diverse UI
+## Node API for Diverse UI
+
+Development in progress. Documentation and unit testing coming soon, please look at example.js for now.
+
+[Diverse UI](http://diverseui.com)
+
+Install with
+
+```sh
+npm install --save diverse-ui
+```
+
+Require as
+```js
+// ES5
+var DiverseUI = require('diverse-ui');
+person.getAll().then(function(data){
+  console.log(data);
+});
+//ES6
+let DiverseUI = require('diverse-ui');
+person.getAll().then((data)=>{
+  console.log(data);
+});
+```
---
+++ example.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let DiverseUI = require('./index');
+let person = new DiverseUI;
+
+// List All DiverseUI Items
+person.getAll().then((data)=>{
+  console.log(data);
+});
+//List All DiverseUI Male Items
+person.getAll("male").then((data)=>{
+  console.log(data);
+});
+// List All DiverseUI Female Items
+person.getAll("female").then((data)=>{
+  console.log(data);
+});
+// List a Random DiverseUI Item
+person.getRandom().then((data)=>{
+  console.log(data);
+});
+// List a Random DiverseUI Male
+person.getRandom("male").then((data)=>{
+  console.log(data);
+});
+// List a Random DiverseUI Female
+person.getRandom("female").then((data)=>{
+  console.log(data);
+});
